953526b7620bcdb4c7a2da1687456faaa1ee41ee
Erik Eelde <373219+erikeelde@users.noreply.github.com>
2025-03-27 13:59:26 +0100

    Bump versions (#227)
    * Bump versions
    
    * Bump gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,15 @@ plugins {
     alias(libs.plugins.nl.littlerobots.version.catalog.update)
     alias(libs.plugins.com.github.ben.manes.versions)
 }
+
+versionCatalogUpdate {
+    sortByKey = true
+    keep {
+        // keep versions without any library or plugin reference
+        keepUnusedVersions = true
+        // keep all libraries that aren't used in the project
+        keepUnusedLibraries = true
+        // keep all plugins that aren't used in the project
+        keepUnusedPlugins = true
+    }
+}
